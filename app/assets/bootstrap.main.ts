37e47cba24cd851dd3e61ca947cb29b686deba66
skalyanmoguloju <kalyansaim@gmail.com>
2017-03-14 11:48:57 -0500

    Master update

--- app/assets/bootstrap.main.ts
+++ app/assets/bootstrap.main.ts
@@ -2,3 +2,6 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"
 import { LabalyticsModule } from "./module/app/app.module"
 
 platformBrowserDynamic().bootstrapModule(LabalyticsModule)
+
+
+
