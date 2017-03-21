ed8387f46cede7ef3ff69d5472ae87b933eca302
MaheshSasidharan <mahesh.sasidharan.1990@gmail.com>
2017-03-20 19:10:14 -0500

    Added routing to login on success of signup

--- app/assets/services/authentication.service.ts
+++ app/assets/services/authentication.service.ts
@@ -1,59 +1,61 @@
-﻿import { Injectable } from '@angular/core';
-import { Http, Headers, Response, RequestOptions } from '@angular/http';
+﻿import {Injectable} from '@angular/core';
+import {Http, Headers, Response, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map'
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
 
 @Injectable()
 export class AuthenticationService {
-    private authenticated: boolean;
-    constructor(public http: Http) {
-      this.authenticated = false;
-    }
+  private authenticated: boolean;
+  private result: Object;
 
-    authorize(username: string, password: string) {
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
-        return this.http.post('/validate', JSON.stringify({ email: username, password: password }), options)
-        //return this.http.post('/registration', JSON.stringify({ email: username, password: password }), options)
-            .map((response: Response) => {
+  constructor(public http: Http) {
+    this.authenticated = false;
+  }
 
-                console.log(response.json());
-                this.authenticated = true;
-                localStorage.setItem('currentUser', JSON.stringify(response.json()));
-                window.location.href = window.location.origin + "/home";;//'http://localhost:9000/home';
-            });
-    }
-
-    private CheckIfAuthenticated(){
-      if(localStorage.getItem('currentUser') === null){
-        this.authenticated = false;
-      }else{
+  authorize(username: string, password: string) {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
+    return this.http.post('/validate', JSON.stringify({email: username, password: password}), options)
+    //return this.http.post('/registration', JSON.stringify({ email: username, password: password }), options)
+      .map((response: Response) => {
         this.authenticated = true;
-      }
+        localStorage.setItem('currentUser', JSON.stringify(response));
+        this.result = response;
+        return this.result;
+      });
+  }
+
+  private CheckIfAuthenticated() {
+    if (localStorage.getItem('currentUser') === null) {
+      this.authenticated = false;
+    } else {
+      this.authenticated = true;
     }
+  }
 
   public isAuthenticated() {
     return this.authenticated;
   }
 
 
-    signup(info: any = {}) {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
+  signup(info: any = {}) {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
     console.log(info);
     // return this.http.post('/validate', JSON.stringify({ email: info, password: password }), options)
-    return this.http.post('/registration', JSON.stringify({ info }), options)
+    return this.http.post('/registration', JSON.stringify({info}), options)
       .map((response: Response) => {
-        console.log(response);
         this.authenticated = true;
         localStorage.setItem('currentUser', JSON.stringify(response));
+        this.result = response;
+        return this.result;
       });
   }
 
-    logout() {
-        // remove user from local storage to log user out
-        //this.authenticated = false;
-        localStorage.removeItem('currentUser');
-    }
+  logout() {
+    // remove user from local storage to log user out
+    //this.authenticated = false;
+    localStorage.removeItem('currentUser');
+  }
 }
