17969f9e9786762f01a26598ff5bec7669529c29
MaheshSasidharan <mahesh.sasidharan.1990@gmail.com>
2017-04-30 01:33:40 -0500

    Added Billing and Equipment report

---
+++ app/assets/components/account/billreport.component.ts
@@ -0,0 +1,45 @@
+import {Component} from "@angular/core"
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {OnInit} from '@angular/core';
+import {AuthenticationService} from "../../services/index";
+import {Message} from 'primeng/primeng';
+
+@Component({
+  selector: "billing-report",
+  templateUrl: "assets/components/account/billreport.component.html"
+})
+
+@Injectable()
+export class BillReportComponent implements OnInit {
+  data: any;
+  msgs: Message[];
+  constructor(public http: Http, private authService: AuthenticationService) {
+
+  }
+
+  ngOnInit() {
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: '#4bc0c0'
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderColor: '#565656'
+        }
+      ]
+    }
+  }
+
+  selectData(event: Event) {
+    this.msgs = [];
+    this.msgs.push({severity: 'info', summary: 'Data Selected', 'detail': this.data.datasets[event.element._datasetIndex].data[event.element._index]});
+  }
+}
---
+++ app/assets/services/helper.service.ts
@@ -0,0 +1,30 @@
+﻿import {Injectable} from '@angular/core';
+
+@Injectable()
+export class HelperService {
+  constructor() {
+  }
+
+  FindItemInArray(array: any [], keyName: string, keyVal: string, returnType: string) {
+    var found = false;
+    if (undefined === keyVal || null === keyVal) {
+      return null;
+    }
+    let nIndex: Number;
+    for (var i in array) {
+      nIndex = i;
+      if (array[i][keyName] == keyVal) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      return null;
+    }
+    if (returnType === "index") {
+      return i;
+    } else {
+      return array[i];
+    }
+  }
+}
