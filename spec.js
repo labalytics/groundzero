0ed38e405ec7de770edde8d6f3fc28a68c62994f
Ankur Shrivastava <ankur-shrivastava@uiowa.edu>
2017-05-01 23:31:24 -0500

    end 2 end testing protractor

--- spec.js
+++ spec.js
@@ -1,7 +1,34 @@
 describe('Labalytics Testing', function() {
   it('should have a title', function() {
     browser.get('http://localhost:9000/');
-    //console.log(browser.getTitle());
+    expect(browser.getTitle()).toEqual('main');
+  });
+});
+
+describe('Labalytics Testing', function() {
+  it('should have a Login', function() {
+    browser.get('http://localhost:9000/authorize#/login');
+    expect(browser.getTitle()).toEqual('login');
+  });
+});
+
+describe('Labalytics Testing', function() {
+  it('should have a Signup', function() {
+    browser.get('http://localhost:9000/authorize#/login');
+    expect(browser.getTitle()).toEqual('login');
+  });
+});
+
+describe('Labalytics Testing', function() {
+  it('should have an about page', function() {
+    browser.get('http://localhost:9000/#about');
+    expect(browser.getTitle()).toEqual('main');
+  });
+});
+
+describe('Labalytics Testing', function() {
+  it('should have an contact page', function() {
+    browser.get('http://localhost:9000/#contact');
     expect(browser.getTitle()).toEqual('main');
   });
 });
